5208a2df3e235103e17f2ad3878b9697ce5d502d
kt24600 <85651377+kt24600@users.noreply.github.com>
2021-06-24 13:26:26 -0700

    Update final_tictactoe.py

--- final_tictactoe.py
+++ final_tictactoe.py
@@ -36,9 +36,9 @@ while True:
 def whoGoesFirst():
     # Randomly choose the player who goes first.
     if random.randint(0, 1) == 0:
-        return 'computer'
+        return 'player1'
     else:
-        return 'player'
+        return 'player2'
 
 def playAgain():
     # This function returns True if the player wants to play again, otherwise it returns False.
